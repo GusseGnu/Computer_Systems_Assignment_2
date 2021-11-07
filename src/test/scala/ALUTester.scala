0009bea7ce75462c99eebd13c74a11c47bb78f02
GusseGnu <gforsingdal@gmail.com>
2021-11-07 23:14:44 +0100

    Updated ALU and Tester as well as ProgramMemory and Tester.

--- src/test/scala/ALUTester.scala
+++ src/test/scala/ALUTester.scala
@@ -6,66 +6,77 @@ class ALUTester(dut: ALU) extends PeekPokeTester(dut) {
   //ALU running for 5 clock cycles
   poke(dut.io.a, 123)
   poke(dut.io.b, 321)
+  poke(dut.io.im, 13)
   poke(dut.io.sel, 0)
   step(5)
 
   //ALU running for another 5 clock cycles
   poke(dut.io.a, 1)
   poke(dut.io.b, 2)
+  poke(dut.io.im, 13)
   poke(dut.io.sel, 1)
   step(5)
 
   //ALU running for another 5 clock cycles
   poke(dut.io.a, 3)
   poke(dut.io.b, 4)
+  poke(dut.io.im, 13)
   poke(dut.io.sel, 2)
   step(5)
 
   //ALU running for another 5 clock cycles
   poke(dut.io.a, 5)
   poke(dut.io.b, 6)
+  poke(dut.io.im, 13)
   poke(dut.io.sel, 3)
   step(5)
 
   //ALU running for another 5 clock cycles
   poke(dut.io.a, 7)
   poke(dut.io.b, 8)
+  poke(dut.io.im, 13)
   poke(dut.io.sel, 4)
   step(1)
 
   //ALU running for another 5 clock cycles
   poke(dut.io.a, 9)
   poke(dut.io.b, 10)
+  poke(dut.io.im, 13)
   poke(dut.io.sel, 5)
   step(5)
 
   //ALU running for another 5 clock cycles
   poke(dut.io.a, 11)
   poke(dut.io.b, 12)
+  poke(dut.io.im, 13)
   poke(dut.io.sel, 6)
   step(5)
 
   //ALU running for another 5 clock cycles
   poke(dut.io.a, 13)
   poke(dut.io.b, 14)
+  poke(dut.io.im, 69)
   poke(dut.io.sel, 7)
   step(5)
 
   //ALU running for another 5 clock cycles
   poke(dut.io.a, 15)
   poke(dut.io.b, 16)
+  poke(dut.io.im, 420)
   poke(dut.io.sel, 8)
   step(5)
 
   //ALU running for another 5 clock cycles
   poke(dut.io.a, 17)
   poke(dut.io.b, 18)
+  poke(dut.io.im, 255)
   poke(dut.io.sel, 9)
   step(5)
 
   //ALU running for another 5 clock cycles
   poke(dut.io.a, 19)
   poke(dut.io.b, 20)
+  poke(dut.io.im, 111)
   poke(dut.io.sel, 10)
   step(5)
 
@@ -73,7 +84,7 @@ class ALUTester(dut: ALU) extends PeekPokeTester(dut) {
 
 object ALUTester {
   def main(args: Array[String]): Unit = {
-    println("Testing Program Counter")
+    println("Testing ALU")
     iotesters.Driver.execute(
       Array("--generate-vcd-output", "on",
         "--target-dir", "generated",
--- src/test/scala/ProgramMemoryTester.scala
+++ src/test/scala/ProgramMemoryTester.scala
@@ -3,39 +3,80 @@ import chisel3.iotesters.PeekPokeTester
 
 class ProgramMemoryTester(dut: ProgramMemory) extends PeekPokeTester(dut) {
 
-  //Program Memory running for 5 clock cycles
-  poke(dut.io.address, 0)
-  step(5)
+//Program Memory running for 5 clock cycles
+poke(dut.io.address, 0)
+poke(dut.io.testerEnable, 1)
+poke(dut.io.testerAddress, 0)
+poke(dut.io.testerWriteEnable, 0)
+poke(dut.io.testerDataWrite, 0)
+step(5)
 
-  //Program Memory running for 5 clock cycles
-  poke(dut.io.address, 1)
-  step(5)
+//Program Memory running for 5 clock cycles
+poke(dut.io.address, 1)
+poke(dut.io.testerEnable, 1)
+poke(dut.io.testerAddress, 1)
+poke(dut.io.testerWriteEnable, 0)
+poke(dut.io.testerDataWrite, 0)
+step(5)
 
-  //Program Memory running for 5 clock cycles
-  poke(dut.io.address, 10)
-  step(5)
+//Program Memory running for 5 clock cycles
+poke(dut.io.address, 2)
+poke(dut.io.testerEnable, 1)
+poke(dut.io.testerAddress, 10)
+poke(dut.io.testerWriteEnable, 0)
+poke(dut.io.testerDataWrite, 0)
+step(5)
 
-  //Program Memory running for 5 clock cycles
-  poke(dut.io.address, 32)
-  step(5)
+//Program Memory running for 5 clock cycles
+poke(dut.io.address, 10)
+poke(dut.io.address, 63)
+poke(dut.io.testerEnable, 1)
+poke(dut.io.testerAddress, 255)
+poke(dut.io.testerWriteEnable, 1)
+poke(dut.io.testerDataWrite, 6969)
+step(5)
 
-  //Program Memory running for 5 clock cycles
-  poke(dut.io.address, 6969)
-  step(5)
+//Program Memory running for 5 clock cycles
+poke(dut.io.address, 32)
+poke(dut.io.address, 1)
+poke(dut.io.testerEnable, 1)
+poke(dut.io.testerAddress, 65535)
+poke(dut.io.testerWriteEnable, 0)
+poke(dut.io.testerDataWrite, 0)
+step(5)
 
-  //Program Memory running for 5 clock cycles
-  poke(dut.io.address, 42069)
-  step(5)
+//Program Memory running for 5 clock cycles
+poke(dut.io.address, 6969)
+poke(dut.io.address, 1)
+poke(dut.io.testerEnable, 0)
+poke(dut.io.testerAddress, 420)
+poke(dut.io.testerWriteEnable, 0)
+poke(dut.io.testerDataWrite, 0)
+step(5)
 
-  //Program Memory running for 5 clock cycles
-  poke(dut.io.address, 65535)
-  step(5)
+//Program Memory running for 5 clock cycles
+poke(dut.io.address, 42069)
+poke(dut.io.address, 1)
+poke(dut.io.testerEnable, 1)
+poke(dut.io.testerAddress, 123)
+poke(dut.io.testerWriteEnable, 0)
+poke(dut.io.testerDataWrite, 4321)
+step(5)
+
+//Program Memory running for 5 clock cycles
+poke(dut.io.address, 65535)
+poke(dut.io.address, 1)
+poke(dut.io.testerEnable, 1)
+poke(dut.io.testerAddress, 321)
+poke(dut.io.testerWriteEnable, 1)
+poke(dut.io.testerDataWrite, 1337)
+step(5)
 
 }
 
 object ProgramMemoryTester {
   def main(args: Array[String]): Unit = {
-    println("Testing Program Counter")
+    println("Testing Program Memory")
     iotesters.Driver.execute(
       Array("--generate-vcd-output", "on",
         "--target-dir", "generated",
