037df3bd6d0fe7c217cc85defaf88d3764d57765
GusseGnu <gforsingdal@gmail.com>
2021-11-07 13:43:23 +0100

    Draft for ALU logic.

--- src/main/scala/ALU.scala
+++ src/main/scala/ALU.scala
@@ -7,6 +7,7 @@ class ALU extends Module {
     val b = Input(UInt(16.W))
     val sel = Input(UInt(16.W))
     val res = Output(UInt(16.W))
+    val comp = Output(UInt(1.W))
   })
 
   // initialise variable names
@@ -14,9 +15,11 @@ class ALU extends Module {
   val b = io.b
   val sel = io.sel
   var res = io.res
+  var comp = io.comp
 
   // Default value for result
   res := 0.U
+  comp = 1.U
 
   // The ALU selection
   switch(sel){ // Op-code
@@ -26,23 +29,27 @@ class ALU extends Module {
     is("b0010".U){res = a - b} // SUBI - Immediate Sub
     is("b0011".U){res = a * b} // MULT - Multiplication
 
-    // Logical
-    is("b0100".U){res = a | b} // OR - Bitwise OR
-    is("b0101".U){res = a & b} // AND - Bitwise AND
-    is("b0110".U){res = !a} // NOT - Bitwise NOT
-
     // Data transfer
-    is("b0111".U){res = } // LI - Load immediate
-    is("b1000".U){res = } // LD - Load data
-    is("b1001".U){res = } // SD - Store data
+    is("b0100".U){res = a} // LI - Load immediate
+    is("b0101".U){res = a} // LD - Load data
+    is("b0110".U){res = a} // SD - Store data
 
     // Branches
-    is("b1010".U){res = } // JR - Jump
-    is("b1011".U){res = } // JEQ - Jump if equal
-    is("b1100".U){res = } // JLT - Jump if less than
-    is("b1101".U){res = } // JGT - Jump if greater than
-    is("b1110".U){res = } // NOP - No operation
-    is("b1111".U){res = } // END - End execution
+    is("b0111".U){res = a; comp = 1.U} // JR - Jump
+    is("b1000".U){res = a; if (a == b) {comp = 1.U}} // JEQ - Jump if equal
+    is("b1001".U){res = a; if (a < b == 1.B) {comp = 1.U}} // JLT - Jump if less than
+    is("b1010".U){res = 65535.U; comp = 1.U} // END - End execution
+
+    
+    //    // Logical
+    //    is("b0100".U){res = a | b} // OR - Bitwise OR
+    //    is("b0101".U){res = a & b} // AND - Bitwise AND
+    //    is("b0110".U){res = !a} // NOT - Bitwise NOT
+
+    // Branches
+    //    is("b1101".U){res = } // JGT - Jump if greater than
+    //    is("b1110".U){res = } // NOP - No operation
+
   }
 
   // https://github.com/schoeberl/chisel-examples/blob/master/src/main/scala/simple/Alu.scala
